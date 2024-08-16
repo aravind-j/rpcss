be90d6f4f7c1b22026b148891d3339b63399677f
J. Aravind <aravindjayaramanagri@gmail.com>
2024-08-16 13:21:31 +0530

    Add print function

---
+++ R/print.pcss.core.R
@@ -0,0 +1,30 @@
+#' Prints summary of \code{pcss.core} object
+#'
+#' \code{pcss.core} prints to console the summary of an object of class
+#' \code{pcss.core} including the dimensionality reduction method used, the
+#' basic details including parameters and the information on the core sets that
+#' can be constituted.
+#'
+#' @param x An object of class \code{pcss.core}.
+#' @param ... Unused
+#' @seealso \code{\link[rpcss]{pcss.core}}
+#'
+#' @export
+print.pcss.core <- function(x, ...){
+
+  cat("\nMethod\n")
+  cat("========================\n")
+
+  print(attr(x, "method"))
+
+  cat("\nDetails\n")
+  cat("========================\n")
+
+  print(x$details)
+
+  cat("\nCore sets\n")
+  cat("=========================\n")
+
+  print(x$cores.info)
+
+}
