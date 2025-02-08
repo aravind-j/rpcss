a10b1e016c146d8a124d77243c2cb3c249b1407f
aravind-j <aravindjayaramanagri@gmail.com>
2025-02-09 00:24:53 +0530

    Add copyright

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,23 @@
+### This file is part of 'rpcss' package for R.
+
+### Copyright (C) 2024-2025, ICAR-NBPGR.
+#
+# rpcss is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# rpcss is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  https://www.r-project.org/Licenses/
+
 ### This file is part of 'EvaluateCore' package for R.
 
-### Copyright (C) 2018-2023, ICAR-NBPGR.
+### Copyright (C) 2018-2025, ICAR-NBPGR.
 #
 # EvaluateCore is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
